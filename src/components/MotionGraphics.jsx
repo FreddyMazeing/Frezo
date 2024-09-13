28d0f50f55a6a78d416068b8f23df63a49e42bc4
FreddyMazeing <freddymaze98@gmail.com>
2024-09-13 03:15:08 +0200

    Web About to finish

--- src/components/MotionGraphics.jsx
+++ src/components/MotionGraphics.jsx
@@ -4,7 +4,8 @@ const MotionGraphics = () => {
   return (
     <div className="flex flex-col max-w-full py-20  uppercase  text-sm font-light ">
       <div className="flex flex-col justify-center items-center max-w-full ">
-        <h1>Motion Graphics</h1>
+        <h1>Website Still Under Development</h1>
+        <h1 className="text-blue-600">Thank You For Visiting Us</h1>
       </div>
     </div>
   );
